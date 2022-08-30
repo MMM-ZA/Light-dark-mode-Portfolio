92d09b0f59572a6c5784d6159cb2a5b522bdf798
makoma mabilo <makoma44@gmail.com>
2022-08-30 13:42:18 +0800

    fixing section transitions

--- app.js
+++ app.js
@@ -1,36 +1,13 @@
-const sections = document.querySelectorAll('.section');
-const sectionNavigation = document.querySelectorAll('.navbar-items');
-const sectionBtn = document.querySelectorAll('.navbar-items ul li');
-const allSections = document.querySelector('.main-wrapper');
 
-function PageTransitions(){
-    //Button click active class
-    for(let i = 0; i < sectionBtn.length; i++){
-        sectionBtn[i].addEventListener('click', function(){
-            let currentBtn = document.querySelectorAll('.navbar-items ul li a ');
-            currentBtn[0].className = currentBtn[0].className.replace('.navbar-items ul li a', '');
-            this.className += ' .navbar-items ul li a ';
+(function () {
+    [...document.querySelectorAll(".nav-btns")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active").classList.remove("active");
+           document.querySelector('[data-id]').classList.add("active");
         })
-    }
+    });
 
-//Active Sections
-  allSections.addEventListener('click', (e) =>{
-        const id = e.target.dataset.id;
-        if(id){
-            //remove selected from the other btns
-            sectionBtn.forEach((btn) =>{
-                btn.classList.remove('active')
-            })
-            e.target.classList.add('active')
-
-            //hide other sections
-            sections.forEach((section)=>{
-                section.classList.remove('active')
-            })
-
-            const element = document.getElementById(id);
-            element.classList.add('active');
-        }
-    })
-  }
-PageTransitions()
+    // document.querySelector(".theme-btn").addEventListener("click", () => {
+    //     document.body.classList.toggle("light-mode");
+    // })
+})();
